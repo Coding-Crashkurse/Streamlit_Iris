d93e74d6d156baca3baab6e9f41622f476194de1
markus <datamastery87@gmail.com>
2021-05-28 16:34:17 +0200

    final dashboard

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 import streamlit as st
 import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
 
 df = pd.read_csv("iris.csv", sep=",")
 
-header = st.beta_container()
-sidebar = st.beta_container()
-main = st.beta_container()
+st.title("Willkommen auf diesem Example Dashboard")
+st.subheader("Analysiere den Iris Datensatz")
+st.write(f"Form des Datensatzes: {df.shape}")
 
-with header:
-    st.title("Willkommen auf diesem Example Dashboard")
-    st.subheader("Analysiere den Iris Datensatz")
+st.sidebar.title("Wähle dein Variablen aus")
+a = st.sidebar.selectbox("Variable A", list(df.columns)[0:4])
+b = st.sidebar.selectbox("Variable B", list(df.columns)[0:4], index=1)
+kmeans_slider = st.sidebar.slider(
+    "Wähle die Anzahl der Centroids", min_value=2, max_value=8
+)
+if a == b:
+    st.sidebar.warning("Variablen sollten nicht identisch sein")
 
-with sidebar:
-    st.sidebar.title("Wähle dein Variablen aus")
-    a = st.sidebar.selectbox("Variable A", list(df.columns)[0:4])
-    b = st.sidebar.selectbox("Variable B", list(df.columns)[0:4], index=1)
-    if a == b:
-        st.sidebar.error('Variablen dürfen nicht identisch sein')
 
-st.dataframe(df.head(10))
+df["cluster"] = KMeans(n_clusters=kmeans_slider, random_state=0).fit_predict(
+    df.loc[:, (a, b)]
+)
+groupsize = df.groupby("cluster").size().reset_index().rename({0: "size"}, axis=1)
+# st.write(groupsize)
 
-st.write(df)
+col1, col2 = st.beta_columns(2)
+
+fig = plt.figure()
+plt.scatter(x=a, y=b, c="cluster", data=df)
+col1.pyplot(fig)
+
+for index, row in groupsize.iterrows():
+    col2.markdown(
+        f'Anzahl in Gruppe <strong>{row["cluster"]}</strong>: <strong>{row["size"]}</strong>.',
+        unsafe_allow_html=True,
+    )
